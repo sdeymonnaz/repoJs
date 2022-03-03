3122dea567f7ed045f6b643512595282acaf25a5
Sebastian Deymonnaz <adeymonn@hotmail.com>
2022-03-03 18:01:27 -0300

    final

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('Hola mundo en JS');
+console.log("Día de cumpleaños: ", 1);
+console.log("Mes de cumpleaños: ", 2);
+console.log("Año de nacimiento: ", 1976);
+console.log("Número favorito", 5);
