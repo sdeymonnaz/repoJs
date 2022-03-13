b6b95e7cc026d01b4df7fa4e515d53b312bfc02d
Sebastian Deymonnaz <adeymonn@hotmail.com>
2022-03-13 14:35:52 -0300

    tp completo

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
-let cantidadDeVentanasEnMiCasa = 6;
-console.log("Cantidad de ventanas: ", cantidadDeVentanasEnMiCasa);
+let noValgoNi5 = 4;
+console.log("noValgoNi5: ", noValgoNi5);
 
-let soyHumano = true;
-console.log("Soy humano: ", soyHumano);
+let miAnioDeNacimiento = 1976;
+let meGustariaTener80Anios = 80;
+let voyATener80AniosElAnio = miAnioDeNacimiento + meGustariaTener80Anios;
+console.log("Voy a tener 80 años en el año: ", voyATener80AniosElAnio);
 
-let miGustoDePizzaPreferido = "calabresa";
-console.log("Mi gusto de pizza preferido: ", miGustoDePizzaPreferido);
+let costoDeUnaTele = 10000;
+let ahorrros = 9000;
+let dineroQueMeFalta = costoDeUnaTele - ahorrros;
+console.log("Dinero que me falta para comprarme una tele: ", dineroQueMeFalta);
 
-let mensaje = false;
-console.log(mensaje);
-mensaje = "Hola";
-console.log(mensaje);
-mensaje = 0;
-console.log(mensaje);
-mensaje = "Minions";
-console.log(mensaje);
+let diasPorAnio = 365;
+let cantidadDeAniosEnUnaDecada = 10;
+let cantidadDeDiasEnUnaDecada = diasPorAnio * cantidadDeAniosEnUnaDecada;
+console.log("Cantidad de dias en una decada: ", cantidadDeDiasEnUnaDecada);
+
+let porcionesPorPizza = 8;
+let porcionesContadas = 24;
+let cantidadDePizzas = porcionesContadas / porcionesPorPizza;
+console.log("Cantidad de pizzas que puedo comer: ", cantidadDePizzas);
+
+let miAnimalFavorite = "perro";
+console.log("Mi animal favorito es un perro: ", miAnimalFavorite == "perro");
+
+console.log("flan vs flan con dulce de leche: ", "flan" != "flan con dulce de leche");
+
+let miEdad = 46;
+let soyMayorDeEdad = miEdad > 17;
+console.log("Soy mayor de edad: ", soyMayorDeEdad);
+
+let soyMenorDe25 = miEdad < 25;
+console.log("Soy menor de 25: ", soyMenorDe25);
+
+let mesActual = "marzo";
+let estamosEnEnero = mesActual === "enero";
+console.log("Estamos en enero: ", estamosEnEnero);
+
+let noEstamosEnEnero = mesActual !== "enero";
+console.log("No estamos en enero: ", noEstamosEnEnero);
+
+let miNotaEnElParcial = 8;
+let notaMinimaParaAprovar = 6;
+let estoyAprobado = miNotaEnElParcial >= notaMinimaParaAprovar;
+console.log("Estoy aprobado: ", estoyAprobado);
+
+let horaActual = 10;
+let esHoraDeDesayunar = horaActual <= 12;
+console.log("Es hora de desayunar: ", esHoraDeDesayunar);
+
